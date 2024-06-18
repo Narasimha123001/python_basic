f44faca2c49c74816d9a98891b118a20a5cd33b5
Narasimha <narasimhaanguluri001@gmail.com>
2024-06-18 13:57:29 +0530

    File:functions

---
+++ functions/function.py
@@ -0,0 +1,11 @@
+def isprime(n):
+    if n > 1:
+        for i in range(2,n):
+            if (n%i)==0:
+                return False
+        else:    
+            return True
+
+p=isprime(45)
+print(p)
+
